44bda6143e73df66f7e4282c93c32d1da949ad8b
Akash <akashdeepmitra221@gmail.com>
2026-07-11 18:15:29 +0530

    I3

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-# ---------------------------------------------------------------------------
-# Configuration for the Flask Auth App
-# ---------------------------------------------------------------------------
-# Update these values to match your local MySQL Community Server setup
-MYSQL_HOST = "mysql-37954bda-akashdeepmitra221-0cb2.c.aivencloud.com"
-MYSQL_USER = "avnadmin"                 # your MySQL userna
-MYSQL_PASSWORD = "AVNS_IYlevqTSgeRhQ2yMJK8"    # your MySQL password
-MYSQL_DB = "auth_app_db"            # the database created by schema.sql
-MYSQL_PORT = 28772                  # default MySQL port
+import os
 
-# Used by Flask to sign session cookies. Change this to any random string.
-ssl_ca="D:/files/ca.pem"
-SECRET_KEY = "aaaaa-bbbbbbb-ccccccc-ddddddd"
+MYSQL_HOST = os.getenv("host")
+MYSQL_USER = os.getenv("user")              # your MySQL username
+MYSQL_PASSWORD =  os.getenv("pas")   # your MySQL password
+MYSQL_DB = os.getenv("db")           # the database created by schema.sql
+MYSQL_PORT =  os.getenv("port")                 # default MySQL port
+SECRET_KEY = os.getenv("secret")
+CA = os.getenv("file")
